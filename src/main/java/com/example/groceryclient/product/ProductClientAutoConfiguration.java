fb72a805fc471752831843ed879e7301b3d0e434
feryadi-amartha <feryadi@amartha.com>
2023-02-06 03:42:07 +0700

    feat: add factory productClient

--- src/main/java/com/example/groceryclient/product/ProductClientAutoConfiguration.java
+++ src/main/java/com/example/groceryclient/product/ProductClientAutoConfiguration.java
@@ -1,4 +1,23 @@
 package com.example.groceryclient.product;
 
+import com.example.groceryclient.customer.CustomerClient;
+import com.example.groceryclient.customer.CustomerClientConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
 public class ProductClientAutoConfiguration {
+    @Bean
+    public ProductClient productClient(ProductClientConfig config) {
+        WebClient webClient = WebClient.builder()
+                .baseUrl(config.getBaseUrl())
+                .build();
+
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
+                .builder(WebClientAdapter.forClient(webClient))
+                .build();
+
+        return httpServiceProxyFactory.createClient(ProductClient.class);
+    }
 }
