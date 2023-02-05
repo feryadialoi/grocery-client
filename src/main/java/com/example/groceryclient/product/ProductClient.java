52fd8e39e2c701894648dc84dc75225393229eae
feryadi-amartha <feryadi@amartha.com>
2023-02-06 03:30:29 +0700

    feat: add request param to getProducts

--- src/main/java/com/example/groceryclient/product/ProductClient.java
+++ src/main/java/com/example/groceryclient/product/ProductClient.java
@@ -3,8 +3,11 @@ package com.example.groceryclient.product;
 import com.example.groceryclient.product.model.GetProductResponse;
 import com.example.groceryclient.product.model.GetProductsResponse;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
+
+import java.util.List;
 
 @HttpExchange("/api/v1/products")
 public interface ProductClient {
@@ -12,5 +15,5 @@ public interface ProductClient {
     GetProductResponse getProduct(@PathVariable Long id);
 
     @GetExchange
-    GetProductsResponse getProducts();
+    GetProductsResponse getProducts(@RequestParam List<Long> productIds);
 }
